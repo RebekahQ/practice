9048d0c5509bdbad08285e4de34c42c6e2134c8c
wq <554928106@qq.com>
2022-01-09 23:03:44 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/practice/demowiki/controller/TestController.java
+++ src/main/java/com/practice/demowiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.practice.demowiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,11 +12,13 @@ public class TestController {
 // GET POST PUT DELETE 查询 新增 修改 删除 restful风格
 // RequestMapping 支持所有请求，支持get 可以用@GetMapping
 // 405 method 不支持
+    @Value("${test.hello:TEST}")
+    private  String testHello;
 
    // @RequestMapping("/hello")
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
-        return "Hello World!";
+        return "Hello World!"+testHello;
     }
     @PostMapping("/hello/post")
     public String helloPost(String name){
