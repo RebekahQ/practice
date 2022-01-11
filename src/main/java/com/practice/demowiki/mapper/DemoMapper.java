d3e3958c689b2ac8ae640976c6b274b96149d25f
wq <554928106@qq.com>
2022-01-11 22:45:39 +0800

    3-5 集成mybatis generator ,生成demo表代码

---
+++ src/main/java/com/practice/demowiki/mapper/DemoMapper.java
@@ -0,0 +1,23 @@
+package com.practice.demowiki.mapper;
+
+import com.practice.demowiki.domain.Demo;
+import com.practice.demowiki.domain.DemoExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface DemoMapper {
+    long countByExample(DemoExample example);
+
+    int deleteByExample(DemoExample example);
+
+    int insert(Demo record);
+
+    int insertSelective(Demo record);
+
+    List<Demo> selectByExample(DemoExample example);
+
+    int updateByExampleSelective(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByExample(@Param("record") Demo record, @Param("example") DemoExample example);
+}
